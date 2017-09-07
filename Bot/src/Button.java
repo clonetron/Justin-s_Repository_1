6e7a0414a96bfd4e4d25b28ff54c89697349004f
Justin <Justin@DESKTOP-VIK8TC0>
2017-09-06 23:41:44 -0700

    hackertyper

---
+++ Bot/src/Button.java
@@ -0,0 +1,203 @@
+
+public class Button {
+	
+	private static String poop = "@echo off\r\n" + 
+			"title SecurityGuard v1.03\r\n" + 
+			"color C\r\n" + 
+			"echo SecurityGuard v1.03 enabled!\r\n" + 
+			"echo This program has been...\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"echo.\r\n" + 
+			"echo     _####_\r\n" + 
+			"echo    #      #\r\n" + 
+			"echo   #        #\r\n" + 
+			"echo   #        #\r\n" + 
+			"echo   #        #\r\n" + 
+			"echo  ############ \r\n" + 
+			"echo #   LOCKED   #\r\n" + 
+			"echo #     ##     #\r\n" + 
+			"echo #    ####    #\r\n" + 
+			"echo #    ####    #\r\n" + 
+			"echo #     ##     #\r\n" + 
+			"echo #     ##     #\r\n" + 
+			"echo ##############\r\n" + 
+			"echo.\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"echo Enter password to activate the program...\r\n" + 
+			"set/p \"pass=>\"\r\n" + 
+			"if NOT %pass%== fakehack goto :incorrect_pass\r\n" + 
+			"if %pass%== fakehack goto :correct_pass</p><p>:correct_pass</p><p>cls\r\n" + 
+			"echo Password hidden...\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"echo PASSWORD CORRECT\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"color 97\r\n" + 
+			"cls\r\n" + 
+			"title svr_hack_var_aggressive.exe\r\n" + 
+			"echo Initializing svr_hack_var_aggressive.exe\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"tree C:/Windows/System32\r\n" + 
+			"echo Initializing Complete! Are you ready to continue enable Proxy Servers?\r\n" + 
+			"@echo off\r\n" + 
+			":choice\r\n" + 
+			"set /P c=Are you sure you want to continue[Y/N]?\r\n" + 
+			"if /I \"%c%\" EQU \"Y\" goto :somewhere\r\n" + 
+			"if /I \"%c%\" EQU \"N\" goto :somewhere_else\r\n" + 
+			"goto :choice</p><p>:somewhere</p><p>cls\r\n" + 
+			"echo Proxy Servers enabling...\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"echo Proxy Servers 27%% enabled...\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"echo Proxy Servers 56%% enabled...\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"echo Proxy Servers 89%% enabled...\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"echo Proxy Servers have been enabled. Are you ready to enable software rooting?\r\n" + 
+			"@echo off\r\n" + 
+			":choice\r\n" + 
+			"set /P c=Are you sure you want to continue[Y/N]?\r\n" + 
+			"if /I \"%c%\" EQU \"Y\" goto :somewhere2\r\n" + 
+			"if /I \"%c%\" EQU \"N\" goto :somewhere_else2\r\n" + 
+			"goto :choice</p><p>:somewhere_else</p><p>echo svr_hack_var_aggressive.exe had been closed, exiting out of cmd...\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"exit</p><p>:somewhere2</p><p>echo Collecting files to root... (Warning, this process may take several minutes!)\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"@echo off\r\n" + 
+			"tree C:\\\r\n" + 
+			"@echo off\r\n" + 
+			":choice\r\n" + 
+			"set /P c=Are you sure you want to root all of the selected files?[Y/N]?\r\n" + 
+			"if /I \"%c%\" EQU \"Y\" goto :somewhere3\r\n" + 
+			"if /I \"%c%\" EQU \"N\" goto :somewhere_else3\r\n" + 
+			"goto :choice</p><p>:somewhere_else2</p><p>echo svr_hack_var_aggressive.exe had been closed, exiting out of cmd...\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"exit</p><p>:somewhere_else3</p><p>echo svr_hack_var_aggressive.exe had been closed, exiting out of cmd...\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"exit</p><p>:somewhere3</p><p>@echo off\r\n" + 
+			"cls\r\n" + 
+			"echo Rooting files... (Warning this process may take several minutes!)\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"echo File rooting has been completed.\r\n" + 
+			"goto :hardcore</p><p>:incorrect_pass</p><p>cls\r\n" + 
+			"echo Password hidden...\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"echo PASSWORD INCORRECT: svr_hack_var_aggressive.exe has been closed, exiting out of cmd...\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"exit</p><p>:hardcore</p><p>@echo off\r\n" + 
+			"cls\r\n" + 
+			"echo Transfering user to passive mode...\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"title Passive\r\n" + 
+			"color B\r\n" + 
+			"echo svr_hack_var_aggressive.exe has been enabled...\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"echo Checking if svr_hack_var_aggressive.exe is running correctly...\r\n" + 
+			"ping -n 2 127.0.0.1>nul \r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"cls\r\n" + 
+			"echo svr_hack_var_aggressive.exe has initialized \r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"echo svr_hack_var_aggressive.exe has enabled Proxy servers \r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"echo svr_hack_var_aggressive.exe has rooted files \r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"cls\r\n" + 
+			"echo Removing traces of svr_hack_var_aggressive.exe in files...\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"tree C:/Windows/ADFS\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"cls\r\n" + 
+			"echo svr_hack_var_aggressive.exe has been removed...\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"echo cmd will be replaced in 3...\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"echo 2...\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"echo 1...\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"cls\r\n" + 
+			"echo ...\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"ping -n 2 127.0.0.1>nul\r\n" + 
+			"cls\r\n" + 
+			"start cmd\r\n" + 
+			"exit</p>";
+	
+	public Button() {
+	}
+		public static void button() {
+			
+			for(int i = 0 ; i < poop.length(); i ++ ) {
+				
+			char c = poop.charAt(i);
+				
+//			Gui.textArea.setText(String.valueOf(c));
+			
+			System.out.print(c);
+			
+			
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+				
+				
+			}
+			
+		}
+}
+//	}
+
+
---
+++ Bot/src/Gui.java
@@ -0,0 +1,73 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Gui {
+
+	private JFrame frame;
+	public static JTextArea textArea;
+
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Gui window = new Gui();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	
+	public Gui() {
+		initialize();
+	}
+
+	
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setLineWrap(true);
+		textArea.setBounds(10, 11, 414, 178);
+		frame.getContentPane().add(textArea);
+		
+		JButton btnNewButton = new JButton("Intiate Windows Hack");
+		btnNewButton.addActionListener(new ActionListener() {					// main method HERE
+			public void actionPerformed(ActionEvent arg0) {
+				
+				
+				Button.button();
+
+					
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+						
+						
+				
+				
+				
+			}
+		});
+		btnNewButton.setBounds(10, 200, 414, 50);
+		frame.getContentPane().add(btnNewButton);
+	}
+}
